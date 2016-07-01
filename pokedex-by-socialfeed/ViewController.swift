825055f9f9571076bbb717368881ed2881d47ec4
Alex <AppDaddy@MacBooks-MacBook-Pro.local>
2016-06-30 22:29:00 -0700

    Added Audio and Custom Font

--- pokedex-by-socialfeed/ViewController.swift
+++ pokedex-by-socialfeed/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +22,27 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collectionView.delegate = self
         collectionView.dataSource = self
     
+        initAudio()
         parsePokemonCSV()
+        
+    }
+    
+    func initAudio(){
+    
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do{
+            
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        }catch let err as NSError {
+            
+            print(err.debugDescription)
+            
+        }
         
     }
 
@@ -86,6 +108,20 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
         return CGSizeMake(105, 105)
     }
+
+    
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        }else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+        
+    }
+    
 
 }
 
