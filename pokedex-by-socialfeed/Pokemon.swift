c31daebfa62245e6e070f1bacdc7cc7512148b51
Alex <AppDaddy@MacBooks-MacBook-Pro.local>
2016-07-02 05:45:58 -0700

    Connected IBOutlets to detailVC and modifed Pokemon model

--- pokedex-by-socialfeed/Pokemon.swift
+++ pokedex-by-socialfeed/Pokemon.swift
@@ -12,6 +12,13 @@ class Pokemon {
 
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionTxt: String!
 
     var name: String {
         return _name
@@ -21,9 +28,45 @@ class Pokemon {
         return _pokedexId
     }
     
-    init(name: String, pokedexId: Int){
+    var description: String {
+        return _description
+    }
+    
+    var type: String {
+        return _type
+    }
+    
+    var defense: String {
+        return _defense
+    }
+    
+    var height: String {
+        return _height
+    }
+    
+    var weight: String {
+        return _weight
+    }
+    
+    var attack: String {
+        return _attack
+    }
+    
+    var nextEvolutionxt: String {
+        return _nextEvolutionTxt
+    }
+    
+    init(name: String, pokedexId: Int, desc: String, type: String, defense: String, height: String, weight: String, attack: String, nextEvo: String){
         self._name = name
         self._pokedexId = pokedexId
+        self._description = desc
+        self._type = type
+        self._defense = defense
+        self._height = height
+        self._weight = weight
+        self._attack = attack
+        self._nextEvolutionTxt = nextEvo
+
     }
     
 }
--- pokedex-by-socialfeed/PokemonDetailVC.swift
+++ pokedex-by-socialfeed/PokemonDetailVC.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         
-        print(pokemon.name)
+        
+    }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
         
     }
     
